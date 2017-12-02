1c1d87208c0effaf460425bca42fbc06f42c4fea
Sterling Sykora <sasykora@uh.edu>
2017-12-02 02:39:30 -0600

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171202060136) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "channels", force: :cascade do |t|
     t.string "name"
     t.text "description"
